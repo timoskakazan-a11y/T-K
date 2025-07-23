79b5a03b410f4be6742ad3d21d455f24a933b8f5
timoskakazan-a11y <timoskakazan@gmail.com>
2025-07-23 20:32:49 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WelcomeBanner from './WelcomeBanner';
+
+function App() {
+  return (
+    <div>
+      <WelcomeBanner />
+    </div>
+  );
+}
+
+export default App;
