45a44f65124265f66dd78802970e7a24b63444ec
timoskakazan-a11y <timoskakazan@gmail.com>
2025-07-24 21:30:05 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'Музыкальное приложение',
+  description: 'Простой плеер на Next.js',
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
